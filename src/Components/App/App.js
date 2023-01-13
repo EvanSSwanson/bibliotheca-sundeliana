ed5b602e414156eee389862386d89dc68f63edf7
Evan Swanson <evan.sundelius@gmail.com>
2023-01-12 22:23:43 -0600

    Furnish most of main page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,6 @@ import "./App.css";
 function App() {
   const [data, setData] = useState([])
   const [favoriteQuotes, setFavoriteQuotes] = useState([])
-
   const getData = (source) => {
     fetch(source)
     .then(response => {
@@ -18,7 +17,7 @@ function App() {
       }
     })
      .then(data => setData(data))
-}
+  }
 
   useEffect(() => {
     getData('https://bible-api.com/john%203:16?translation=kjv')
@@ -27,20 +26,20 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {data.text}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <img src="https://www.gannett-cdn.com/presto/2019/04/16/USAT/ec6b48e5-214b-46c4-bd4d-d5ebf411fa14-GettyImages-636538866.jpg?width=1320&height=880&fit=crop&format=pjpg&auto=webp" className="header-photo" alt="logo" /> */}
+        <div className="banner-top"></div>
+        <div className="banner">
+          <div className="title-border">
+            <h1 className="title">BIBLIOTHECA SUNDELIANA</h1>
+          </div>
+        </div>
+        <div className="banner-bottom"></div>
+        <div className="selection-section">
+          <button className="favorites"></button>
+          <button className="filter"></button>
+          <button className="theme"></button>
+        </div>
+        <p className="about">About</p>
     </div>
   );
 }
