e70982f46eade0c109b5ab3e287e2da29b8c418e
Evan Swanson <evan.sundelius@gmail.com>
2023-01-12 19:19:39 -0600

    Connect API to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,36 @@
-import logo from '../../logo.svg';
-import './App.css';
+import logo from "../../logo.svg";
+import React, { useState, useEffect } from "react"
+//import getData from "../../apicalls"
+import { Route, Routes, NavLink } from "react-router-dom"
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([])
+  const [favoriteQuotes, setFavoriteQuotes] = useState([])
+
+  const getData = (source) => {
+    fetch(source)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong ...');
+      }
+    })
+     .then(data => setData(data))
+}
+
+  useEffect(() => {
+    getData('https://bible-api.com/john%203:16?translation=kjv')
+    console.log("data has loaded!", data)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.text}
         </p>
         <a
           className="App-link"
