f63fb98ccdeb238bd41e078f98e678c822b460e1
Evan Swanson <evan.sundelius@gmail.com>
2023-01-13 16:14:02 -0600

    Set up Router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,22 +25,35 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <main className="App">
+      <Routes>
+        <Route path="/" element={<div className="home-page">
+            <div className="banner-top"></div>
+            <div className="banner">
+              <div className="title-border">
+                <h1 className="title">BIBLIOTHECA SUNDELIANA</h1>
+              </div>
+            </div>
+            <div className="banner-bottom"></div>
+            <div className="selection-section">
+              <NavLink to='/favorites' className='favorites-link'>
+                <button className="favorites"></button>
+              </NavLink>
+              <NavLink to='/filter' className='filter-link'>
+                <button className="filter"></button>
+              </NavLink>
+              <NavLink to='/theme' className='theme-link'>
+                <button className="theme"></button>
+              </NavLink>
+            </div>
+            <p className="about">About</p>
+          </div>}
+        />
+        <Route
+        />
+      </Routes>
         {/* <img src="https://www.gannett-cdn.com/presto/2019/04/16/USAT/ec6b48e5-214b-46c4-bd4d-d5ebf411fa14-GettyImages-636538866.jpg?width=1320&height=880&fit=crop&format=pjpg&auto=webp" className="header-photo" alt="logo" /> */}
-        <div className="banner-top"></div>
-        <div className="banner">
-          <div className="title-border">
-            <h1 className="title">BIBLIOTHECA SUNDELIANA</h1>
-          </div>
-        </div>
-        <div className="banner-bottom"></div>
-        <div className="selection-section">
-          <button className="favorites"></button>
-          <button className="filter"></button>
-          <button className="theme"></button>
-        </div>
-        <p className="about">About</p>
-    </div>
+    </main>
   );
 }
 
